c01366cece6df46567783227e357aee8c14f5a33
Rouslan Lakhno <rouslanlakhno@gmail.com>
2024-01-22 19:17:53 -0500

    Initial commit

---
+++ spinner1.js
@@ -0,0 +1,45 @@
+process.stdout.write('-> d2 spinner1.js\n');
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1300);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   ');
+}, 1500);
+
+setTimeout(() => {
+  process.stdout.write('\r|   \n');
+}, 1700);
+
+setTimeout(() => {
+  process.stdout.write('-> d2   \n');
+}, 1900);
+
+// ... fill in the rest yourself ...
